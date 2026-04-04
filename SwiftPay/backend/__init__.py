ab6cfc3fd88484ad0ac7569c1d2a1d15f1cd2c71
daviRolvr <daviemanueloliveira7@gmail.com>
2026-04-04 20:32:02 -0300

    Adicionar arquivo __init__.py com informações do módulo backend do SwiftPay

---
+++ SwiftPay/backend/__init__.py
@@ -0,0 +1,12 @@
+"""
+SwiftPay - Sistema de Auditoria de Transações Financeiras
+
+Módulo backend contendo:
+- app.py: Aplicação Flask com API REST
+- search_algorithms.py: Implementação de algoritmos de busca
+- data/transactions.json: Base de dados das transações
+- frontend/: Interface web do projeto
+"""
+
+__version__ = "1.0.0"
+__author__ = "Estudante de Engenharia de Software"
